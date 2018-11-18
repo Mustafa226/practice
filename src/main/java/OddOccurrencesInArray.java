8aa573b07da2809d16a7025ca54ebaa47196711e
BattleArray61 <mehuljic.mustafa@gmail.com>
2018-11-18 21:44:00 +0300

    Odd Occurences in Array is implemented

--- src/main/java/OddOccurrencesInArray.java
+++ src/main/java/OddOccurrencesInArray.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -12,10 +10,8 @@ public class OddOccurrencesInArray {
         if ((N % 2 != 1) || (N <= 0) || (N > 1000000)) {
             return result;
         } else {
-            List<Integer> list = new ArrayList<>();
-            Arrays.stream(A).forEach(list::add);
-            return list.stream().collect(Collectors.groupingBy(value -> value)).entrySet()
-                    .stream().filter(entry -> entry.getValue().size() == 1)
+            return Arrays.stream(A).boxed().collect(Collectors.groupingBy(value -> value))
+                    .entrySet().stream().filter(entry -> entry.getValue().size() == 1)
                     .map(Map.Entry::getKey).findAny().orElse(0);
         }
     }
